92bccf947fb0ac52e773a48a5404cd2c9dda4d4c
Tran Anh Vu <tran.anh.vu@framgia.com>
2017-10-10 11:06:55 +0700

    Remove Ebean configuration

--- app/ebean/EbeanRepository.java
+++ app/ebean/EbeanRepository.java
@@ -1,6 +1,6 @@
 package ebean;
 
-import io.ebean.Finder;
+//import io.ebean.Finder;
 import models.SUser;
 
 import java.util.List;
@@ -8,7 +8,7 @@ import java.util.List;
 public class EbeanRepository {
 
     private static List<SUser> getUserList(){
-        public static final Finder<Long, SUser> find = new Finder<>(SUser.class);
+//        public static final Finder<Long, SUser> find = new Finder<>(SUser.class);
         return null;
     }
 }
